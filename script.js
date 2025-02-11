470a0d6ffead19b7d56f3666ce1e190567e738d2
Aditya Surapaneni <asurap1426@gmail.com>
2025-02-11 14:48:30 -0800

    Loaded data from JSON using JS.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+let mouseData = [];
+
+d3.json("mouse_data.json").then(data => {
+    mouseData = data;
+    console.log("Loaded the data.", mouseData.slice(0, 10));
+}).catch(error => console.error("Error loading the data.", error));
+
+function filterDataByGender(gender) {
+    return mouseData.filter(d => {
+        if (gender === "female") {
+            return d.mouse_id.startsWith("f");
+        } else if (gender === "male") {
+            return d.mouse_id.startsWith("m");
+        }
+        return false;
+    });
+}
+
+console.log("Sample mouse IDs:", mouseData.map(d => d.mouse_id).slice(0, 10));
